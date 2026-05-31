79972df05c8485c23f087924b951eb8f13ea3fe3
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 21:37:34 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class clusterVecryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = clusterVecryptWorker()
+    print("Worker engine initialized.")
